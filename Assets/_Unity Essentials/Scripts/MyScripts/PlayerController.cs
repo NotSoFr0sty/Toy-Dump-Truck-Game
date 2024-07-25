e6b4dfddc9cccdd65a12b9deaf5ad157bd15a5dc
Tom Harris Pazhayattil <tomharrisp@gmail.com>
2024-07-25 20:04:20 +0530

    Add jumping and turning to PlayerController

--- Assets/_Unity Essentials/Scripts/MyScripts/PlayerController.cs
+++ Assets/_Unity Essentials/Scripts/MyScripts/PlayerController.cs
@@ -4,7 +4,11 @@ public class PlayerController : MonoBehaviour {
 
     private Rigidbody rb;
     public float speed = 5.0f;
-    public float turnSpeed = 120.0f;
+    public float rotationSpeed = 120.0f;
+    public float jumpForce = 1.0f;
+    public bool canJump;
+    public bool canRight;
+
     private bool stunting; // Whether the player is flipping in the air or not.
     private Vector3 forwardDirectionWhileStunting; // Holds the last transform.forward of the player right before stunting
 
@@ -12,12 +16,18 @@ public class PlayerController : MonoBehaviour {
     void Start() {
 
         rb = GetComponent<Rigidbody>();
-        stunting = false;
+        // stunting = false;
+        // canJump = false;
+        // canRight = false;
     }
 
     // Update is called once per frame
     void Update() {
 
+        if (Input.GetButtonDown("Jump") && (canJump || canRight)) {
+
+            rb.AddForce(jumpForce * Vector3.up, ForceMode.VelocityChange);
+        }
     }
 
     private void FixedUpdate() {
@@ -33,5 +43,10 @@ public class PlayerController : MonoBehaviour {
             forwardMovement = speed * moveVertical * Time.fixedDeltaTime * forwardDirectionWhileStunting;
         }
         rb.MovePosition(transform.position + forwardMovement);
+
+        // Rotate player based on Horizontal input
+        float turn = Input.GetAxis("Horizontal") * rotationSpeed * Time.fixedDeltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        rb.MoveRotation(rb.rotation * turnRotation);
     }
 }
