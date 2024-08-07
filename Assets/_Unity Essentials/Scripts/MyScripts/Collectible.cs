0fa851132bc7323a423771d0413f744e7aa5c991
Tom Harris Pazhayattil <tomharrisp@gmail.com>
2024-08-07 17:28:06 +0530

    Create a Menu scene

--- Assets/_Unity Essentials/Scripts/MyScripts/Collectible.cs
+++ Assets/_Unity Essentials/Scripts/MyScripts/Collectible.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Collectible : MonoBehaviour {
 
     public float RotationSpeed = 1.0f;
     public GameObject onCollectParticleEffect;
     private PlayerController player;
+    private Scene currentScene;
 
     // Start is called before the first frame update
     void Start() {
         player = GameObject.Find("Player").GetComponent<PlayerController>();
+        currentScene = SceneManager.GetActiveScene();
     }
 
     // Update is called once per frame
@@ -27,7 +30,8 @@ public class Collectible : MonoBehaviour {
 
         if (other.gameObject.CompareTag("Player")) {
 
-            player.AddToScore(1);
+            if (!(currentScene.buildIndex == 0)) // Add to score only if the current scene is not the menu scene
+                player.AddToScore(1);
             Destroy(gameObject);
             Instantiate(onCollectParticleEffect, transform.position, transform.rotation);
         }
